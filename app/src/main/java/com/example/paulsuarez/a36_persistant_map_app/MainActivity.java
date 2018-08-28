ecd79234be5cc6be92127f2421373287933ef4a7
Paul Suarez <paulsblogging.com@gmail.com>
2018-08-28 10:56:25 -0700

    structured and working on the api key

---
+++ app/src/main/java/com/example/paulsuarez/a36_persistant_map_app/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.paulsuarez.a36_persistant_map_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
